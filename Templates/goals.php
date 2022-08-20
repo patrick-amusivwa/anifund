249b74d632b460940d663e77033d60724df38070
quaint <victorquaint@gmail.com>
2022-08-20 23:00:46 +0300

    Templates

---
+++ Templates/goals.php
@@ -0,0 +1,53 @@
+<section id="our-goals">
+    <div class="section-title">
+        <h2>Our Mission</h2>
+    </div>
+    <div class="row">
+        <div class="col-md-6 col-lg-4">
+            <div id="left-about">
+                <div class="left-heading">
+                    <h3><b>Loving Care</b></h3>
+                    <br>
+                    <p>Our <b>state of the art rehabilitation facility</b> gives every pet that comes to us
+                        a fighting chance.
+                        With proper medical attention and training, we believe every pet can be transformed
+                        into the perfect addition
+                        to any family</p>
+                </div>
+                <div class="left-heading">
+                    <h3><b>Second Chances</b></h3>
+                    <br>
+                    <p>Here at <b>Paws and Claws</b>, we believe in second chances.
+                        That is why we will never put a healthy pet down.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4 d-none d-lg-block ">
+            <div id="center-about">
+                <span role="img" aria-label="golden retreiver with tulip in its mouth">
+                </span>
+            </div>
+        </div>
+        <div class="col-md-6 col-lg-4">
+            <div id="right-about">
+                <div class="right-heading">
+                    <h3><b>Education for All</b></h3>
+                    <br>
+                    <p>We understand how hard it can be adjusting to your new life as a pet owner, which is
+                        why we are always available for <b>advice, support and encouragement</b>.
+                        Just give us a ring, or an email!
+                    </p>
+                </div>
+                <div class="right-heading">
+                    <h3><b>Forever Homes</b></h3>
+                    <br>
+                    <p>Pets can make a house a home. After being through so much, we believe every pet that
+                        we treat deserves a <b>home to call their own.</b>
+                        We spend as much time and effort as possible placing our animals with loving
+                        families, who will cherish them forever.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ Templates/header.php
@@ -0,0 +1,47 @@
+<header>
+    <nav class="navbar navbar-expand-lg navbar-custom fixed-top">
+        <a class="navbar-brand" href="index.html">animal crowdfund <i class="fas fa-paw" aria-hidden="true"></i></a>
+        <button class="navbar-toggler navbar-light" type="button" data-toggle="collapse"
+            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.html" aria-label="link to home page">Home<span
+                            class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.html" aria-label="link to about page">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.html" aria-label="link to contact page">contact</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle navbarDropdown" id="navbarDropdownSuccessStories" href="#"
+                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Success Stories
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownSuccessStories">
+                        <a class="dropdown-item" href="marley.html" aria-label="link to Marley's story">Marley</a>
+                        <a class="dropdown-item" href="lassie.html" aria-label="link to Lassie's story">Lassie</a>
+                        <a class="dropdown-item" href="felix.html" aria-label="link to Felix's story">Felix</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle navbarDropdown" id="navbarDropdownSupportUs" href="#"
+                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Support Us
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownSupportUs">
+                        <a class="dropdown-item" href="https://paypal.me/graperavies?locale.x=en_GB" target="_blank"
+                            rel="noopener" aria-label="link to paypal for donations">Donate</a>
+                        <a class="dropdown-item" href="contact.html"
+                            aria-label="link to volunteer/contact us form">Volunteer</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
