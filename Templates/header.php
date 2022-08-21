d0b6a85e0763e1006251c85465941e1fe9dd48a4
patrick amusivwa <108129819+patrick-amusivwa@users.noreply.github.com>
2022-08-21 14:44:07 +0300

    update header

--- Templates/header.php
+++ Templates/header.php
@@ -1,47 +1,53 @@
 <header>
-    <nav class="navbar navbar-expand-lg navbar-custom fixed-top">
-        <a class="navbar-brand" href="index.html">animal crowdfund <i class="fas fa-paw" aria-hidden="true"></i></a>
-        <button class="navbar-toggler navbar-light" type="button" data-toggle="collapse"
-            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="index.html" aria-label="link to home page">Home<span
-                            class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="about.html" aria-label="link to about page">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="contact.html" aria-label="link to contact page">contact</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle navbarDropdown" id="navbarDropdownSuccessStories" href="#"
-                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Success Stories
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownSuccessStories">
-                        <a class="dropdown-item" href="marley.html" aria-label="link to Marley's story">Marley</a>
-                        <a class="dropdown-item" href="lassie.html" aria-label="link to Lassie's story">Lassie</a>
-                        <a class="dropdown-item" href="felix.html" aria-label="link to Felix's story">Felix</a>
-                    </div>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle navbarDropdown" id="navbarDropdownSupportUs" href="#"
-                        role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Support Us
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownSupportUs">
-                        <a class="dropdown-item" href="https://paypal.me/graperavies?locale.x=en_GB" target="_blank"
-                            rel="noopener" aria-label="link to paypal for donations">Donate</a>
-                        <a class="dropdown-item" href="contact.html"
-                            aria-label="link to volunteer/contact us form">Volunteer</a>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    </nav>
-</header>
+        <nav class="navbar navbar-expand-lg navbar-custom fixed-top">
+            <a class="navbar-brand" href="index.html">animal crowdfund <i class="fas fa-paw" aria-hidden="true"></i></a>
+            <a class="navbar-brand" class="login" href="index.html"><i class="fa-solid fa-user-lock" aria-hidden="true"></i></a>
+            <div class="group">
+                <svg class="icon" aria-hidden="true" viewBox="0 0 24 24"><g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g></svg>
+                <input placeholder="Search" type="search" class="input">
+              </div>
+
+            <button class="navbar-toggler navbar-light" type="button" data-toggle="collapse"
+                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.html" aria-label="link to home page">Home<span
+                                class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.html" aria-label="link to about page">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.html" aria-label="link to contact page">contact</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle navbarDropdown" id="navbarDropdownSuccessStories" href="#"
+                            role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Success Stories
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownSuccessStories">
+                            <a class="dropdown-item" href="marley.html" aria-label="link to Marley's story">Marley</a>
+                            <a class="dropdown-item" href="lassie.html" aria-label="link to Lassie's story">Lassie</a>
+                            <a class="dropdown-item" href="felix.html" aria-label="link to Felix's story">Felix</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle navbarDropdown" id="navbarDropdownSupportUs" href="#"
+                            role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Support Us
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownSupportUs">
+                            <a class="dropdown-item" href="https://paypal.me/graperavies?locale.x=en_GB" target="_blank"
+                                rel="noopener" aria-label="link to paypal for donations">Donate</a>
+                            <a class="dropdown-item" href="contact.html"
+                                aria-label="link to volunteer/contact us form">Volunteer</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
